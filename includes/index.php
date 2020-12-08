3e777ba7a962388acf129024cfae409d713c0732
tonyinfo <info@tjicom.com>
2020-12-08 18:09:08 +0000

    includes

---
+++ includes/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<?php
+include "header.html";
+
+echo "I can do all things through Christ who strengthens me. Praise God!";
+
+
+include "footer.html";
+?>
+
+
+</body>
+</html>
