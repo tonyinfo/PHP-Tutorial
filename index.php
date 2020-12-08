28204226c47a202f8b3404d1a98aaa8aa18a145d
tonyinfo <info@tjicom.com>
2020-12-08 17:14:15 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<html>
+  <head>
+    <title>php</title>
+  </head>
+  <body>
+
+    <?php
+        echo "We can do all things through Christ who strengthens us! Praise God!";
+     ?>
+
+    <p>This is the landing page of <strong>your_domain</strong>.</p>
+  </body>
+</html>
