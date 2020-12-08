aad5924e33ce711e9d5681ffcbc7f13e4ab6df60
tonyinfo <info@tjicom.com>
2020-12-08 17:05:38 +0000

    switch.php

---
+++ switch.php
@@ -0,0 +1,25 @@
+<h1>Switch Statement</h1>
+
+
+<?php
+$grade = "A";
+switch($grade) {
+case A:
+echo "You are excellent!";
+break;
+case B:
+echo "You are wonderful!";
+break;
+case C:
+echo "You are good!";
+break;
+case D:
+echo "You are good but could do more!";
+break;
+case E:
+echo "That is not good enough!";
+break;
+default:
+echo "Check your answer.";
+}
+?>
