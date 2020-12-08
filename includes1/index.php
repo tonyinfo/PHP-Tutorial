fc55ba5eab60dd91b1db830c79dc3de63dac0c71
tonyinfo <info@tjicom.com>
2020-12-08 19:01:43 +0000

    includes1

---
+++ includes1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<?php
+include "header.html";
+
+include "body.html";
+
+include "footer.html";
+?>
+
+
+</body>
+</html>
